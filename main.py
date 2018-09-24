1bdc557264746f066f2c51d454bd8f2169597f31
bilnkat <billyjohn.general@gmail.com>
2018-09-23 18:29:41 -0700

    1

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import subprocess as sp
 sp.call('netsh wlan show profiles')
 output = sp.check_output('netsh wlan show profiles')
 print(output)
+x = output.split()
+y = (x[-1])
+print(y.decode('utf-8'))
