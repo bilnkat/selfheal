4a9d6b48ade910bd637a7f8bd410e3364e43eb3a
bilnkat <billyjohn.general@gmail.com>
2018-09-25 23:16:45 -0700

    added proper var and def

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import subprocess as sp
+import ctypes
 
-sp.call('netsh wlan show profiles')
-output = sp.check_output('netsh wlan show profiles')
-print(output)
-x = output.split()
-y = (x[-1])
-print(y.decode('utf-8'))
+guestSSID = 'CJ'
+currentSSID = ''
+
+def getCurrentSSID():
+    global currentSSID
+    output = sp.check_output('netsh wlan show interface')
+    x = output.split()
+    try:
+        i = x.index(b'SSID')
+        y = (x[i + 2])
+        currentSSID = y.decode('utf-8')
+    except ValueError:
+        ctypes.windll.user32.MessageBoxW(0, "Oops!  Please connect to a WiFi network...", "Error!", 0)
+        print("Oops!  Please connect to a WiFi network...")
+
+
+
+def ssidChecker(ssid):
+    global currentSSID, guestSSID
+    if currentSSID == guestSSID:
+        ssidAlert(ssid)
+
+def ssidAlert(wifi):
+    ctypes.windll.user32.MessageBoxW(0, "You are connected to " + wifi + ". To access internal sites, please connect to MULE.", "Suggestion", 0)
+
+if __name__ == '__main__':
+    getCurrentSSID()
+    ssidChecker(guestSSID)
